8f5dfd1d07065582a7cec4acb697f2c5321bc353
Marion Grandjean <grandjean.marion@gmail.com>
2015-06-24 15:23:05 +0200

    conflict solving

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20150624090448) do
-=======
 ActiveRecord::Schema.define(version: 20150624095621) do
->>>>>>> 2f758526cb072f0dccc997f68205f1a13487220e
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,22 +17,6 @@ User.destroy_all
 Flat.destroy_all
 Booking.destroy_all
 
-<<<<<<< HEAD
-User.destroy_all
-Flat.destroy_all
-
-  User.create!(email: 'palo@alto.com', password: '123456789', password_confirmation: '123456789')
-  User.create!(email: 'gerard@caramail.fr', password: '123456789', password_confirmation: '123456789')
-  User.create!(email: 'Gertrude@caramail.fr', password: '123456789', password_confirmation: '123456789')
-  User.create!(email: 'Bernard@caramail.fr', password: '123456789', password_confirmation: '123456789')
-  User.create!(email: 'Bernadette@caramail.fr', password: '123456789', password_confirmation: '123456789')
-  User.create!(email: 'Milady@caramail.fr', password: '123456789', password_confirmation: '123456789')
-
- Flat.create!(title: "Maison de fou", address: "32, rue blabla", city: "Trifoullis", price_per_night: 20, capacity: 17)
- Flat.create!(title: "Gîte délabré charmant", address: "65 rue Bled", city: "Oise", price_per_night: 420, capacity: 3)
- Flat.create!(title: "Cave rurale", address: "32, rue souli", city: "Trifoullis", price_per_night: 120, capacity: 1)
- Flat.create!(title: "Maison de campagne", address: "2, ravenue des tilleuls", city: "Germines", price_per_night: 30, capacity: 7)
-=======
 User.create!(email: 'palo@alto.com', password: '123456789', password_confirmation: '123456789')
 User.create!(email: 'gerard@caramail.fr', password: '123456789', password_confirmation: '123456789')
 User.create!(email: 'Gertrude@caramail.fr', password: '123456789', password_confirmation: '123456789')
@@ -46,4 +30,3 @@ Flat.create!(title: "Cave rurale", address: "32, rue souli, Trifoullis", price_p
 Flat.create!(title: "Maison de campagne", address: "2, ravenue des tilleuls, Germines", price_per_night: 30, capacity: 7)
 
 Booking.create!(user_id: User.first.id, flat_id: Flat.first.id, nb_travelers: 2, start_date: "10 juillet 2015", end_date: "12 aout 2015", message:"I love your flat", status: "pending")
->>>>>>> 2f758526cb072f0dccc997f68205f1a13487220e
