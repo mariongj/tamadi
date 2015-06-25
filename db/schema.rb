3d638535c6d34b4a4695e0f823139337cf38fd6f
Marion Grandjean <grandjean.marion@gmail.com>
2015-06-25 15:36:11 +0200

    added user pic

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150625095832) do
+ActiveRecord::Schema.define(version: 20150625132918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,10 +64,13 @@ ActiveRecord::Schema.define(version: 20150625095832) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
-    t.string   "picture"
     t.string   "name"
     t.string   "token"
     t.datetime "token_expiry"
+    t.string   "picture_file_name"
+    t.string   "picture_content_type"
+    t.integer  "picture_file_size"
+    t.datetime "picture_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
