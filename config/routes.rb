3e1166b27888fec13ef7489c15bb626119f6afb8
Dimitri Boukobza <dimitri.boukobza@gmail.com>
2015-06-26 12:41:54 +0200

    editing profile possible

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   resources :flats do
     collection do
